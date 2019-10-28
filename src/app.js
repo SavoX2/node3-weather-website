90613316c964fd97bc5a13fdd06aeb66f605d94d
Savo Debeljak <savo.debeljak96@gmail.com>
2019-10-28 12:57:23 +0100

    Refactor app.js for git testing purposes.

--- src/app.js
+++ src/app.js
@@ -18,13 +18,17 @@ hbs.registerPartials(partialsDir);
 app.use(express.static(publicDir));
 
 app.get('/weather', (req, res) => {
-    if(!req.query.address) {
+    if (!req.query.address) {
         return res.send({
             error: 'Address must be provided'
         });
     }
 
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    geocode(req.query.address, (error, {
+        latitude,
+        longitude,
+        location
+    } = {}) => {
         if (error) {
             return res.send({
                 error
@@ -41,13 +45,13 @@ app.get('/weather', (req, res) => {
                 location,
                 address: req.query.address,
             });
-            
+
         });
     });
 });
 
 app.get('/products', (req, res) => {
-    if(!req.query.search) {
+    if (!req.query.search) {
         return res.send({
             error: 'You must provide a search term'
         });
@@ -59,38 +63,38 @@ app.get('/products', (req, res) => {
 
 app.get('', (req, res) => {
     res.render('index', {
-        title: 'Weather App', 
-        name: 'Savo Debeljak', 
+        title: 'Weather App',
+        name: 'Savo Debeljak',
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About', 
-        name: 'Savo Debeljak', 
+        title: 'About',
+        name: 'Savo Debeljak',
     });
 });
 
 app.get('/help', (req, res) => {
     res.render('help', {
         message: 'You is fucked',
-        title: 'Help', 
+        title: 'Help',
         name: 'Savo Debeljak'
     });
 });
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
-        title: '404', 
-        name: 'Debeljak Savo', 
+        title: '404',
+        name: 'Debeljak Savo',
         errorMessage: 'Help article not found'
     });
 });
 
 app.get('*', (req, res) => {
     res.render('404', {
-        title: '404', 
-        name: 'Debeljak Savo', 
+        title: '404',
+        name: 'Debeljak Savo',
         errorMessage: 'Page not found'
     });
 });
